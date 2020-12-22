4d553a22d4b44694b2287a2899c01c39ee45721a
Brian Burkard <brianburkard@gmail.com>
2020-12-21 23:24:36 -0500

    Updated front page and fonts

--- front-page.php
+++ front-page.php
@@ -4,13 +4,12 @@
 <section class="page-wrap frame-inner">
     <main class="about">
         <!-- <h1><?php the_title();?></h1> -->
-        <?php the_post_thumbnail( 'medium' ); ?>
+        <div class="round-image">
+            <?php the_post_thumbnail( 'medium' ); ?>
+        </div>
         <div>
             <?php get_template_part('includes/section', 'content');?>
         </div>
-         <!-- <div class="form">
-        <?php get_template_part('includes/form','inquiry');?>
-    </div> -->
     </main>
 </section>
 
